118b2c4482a85dedb27f48613a5aeaa1a737b846
liangyumei08359 <1093841033@qq.com>
2017-11-18 20:29:33 +0800

    First commit

---
+++ app/src/main/java/com/example/coolweather/db/Country.java
@@ -0,0 +1,36 @@
+package com.example.coolweather.db;
+
+/**
+ * Created by liangyumei on 2017/11/18.
+ */
+
+public class Country extends Datasupport{
+    private int id;
+    private String countryName;
+    private String weatherId;
+    private int cityId;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getCountryName(){
+        return countryName;
+    }
+    public void setCountryName(String countryName){
+        this.countryName=countryName;
+    }
+    public String getWeatherId(){
+        return weatherId;
+    }
+    public void setWeatherId(String weatherId){
+        this.weatherId=weatherId;
+    }
+    public int getCityId(){
+        return cityId;
+    }
+    public void setCityId(int cityId){
+        this.cityId=cityId;
+    }
+}
